965191c5de7bc519b206c060e18c084f7f0eb043
elidacon <elidacon@student.21-school.ru>
2023-11-23 14:46:03 +0000

    Reduce grep tests to 1 to find a problem with pipeline

--- src/C2_SimpleBashUtils/grep/tests.sh
+++ src/C2_SimpleBashUtils/grep/tests.sh
@@ -38,35 +38,35 @@ tests=(
 )
 
 manual=(
-    ""
+    # ""
     "-n for test_files/test_1_grep.txt test_files/test_2_grep.txt"
-    "-n for test_files/test_1_grep.txt"
-    "-n -e ^\} test_files/test_1_grep.txt"
-    "-c -e /\ test_files/test_1_grep.txt"
-    "-ce ^int test_files/test_1_grep.txt test_files/test_2_grep.txt"
-    "-e ^int test_files/test_1_grep.txt"
-    "-nivh = test_files/test_1_grep.txt test_files/test_2_grep.txt"
-    # "-e"
-    "-ie INT test_files/test_5_grep.txt"
-    "-echar test_files/test_1_grep.txt test_files/test_2_grep.txt"
-    "-ne = -e out test_files/test_5_grep.txt"
-    "-iv int test_files/test_5_grep.txt"
-    "-in int test_files/test_5_grep.txt"
-    "-c -l aboba test_files/test_1_grep.txt test_files/test_5_grep.txt"
-    "-v test_files/test_1_grep.txt -e ank"
-    "-noe "\)" test_files/test_5_grep.txt"
-    "-l for test_files/test_1_grep.txt test_files/test_2_grep.txt"
-    "-o -e int test_files/test_4_grep.txt"
-    "-o -e int -e in -e i test_files/test_1_grep.txt"
-    "-o -e in -e i -e int test_files/test_1_grep.txt"
-    "-e = -e out test_files/test_5_grep.txt"
-    "-noe ing -e as -e the -e not -e is test_files/test_6_grep.txt"
-    "-e ing -e as -e the -e not -e is test_files/test_6_grep.txt"
-    "-c -e . test_files/test_1_grep.txt -e '.'"
-    "-l for no_file.txt test_files/test_2_grep.txt"
-    "-e int -si no_file.txt grep.c no_file2.txt grep.h"
-    "-si grep.c -f no_pattern.txt"
-    "-f test_files/test_3_grep.txt test_files/test_5_grep.txt"
+    # "-n for test_files/test_1_grep.txt"
+    # "-n -e ^\} test_files/test_1_grep.txt"
+    # "-c -e /\ test_files/test_1_grep.txt"
+    # "-ce ^int test_files/test_1_grep.txt test_files/test_2_grep.txt"
+    # "-e ^int test_files/test_1_grep.txt"
+    # "-nivh = test_files/test_1_grep.txt test_files/test_2_grep.txt"
+    # # "-e"
+    # "-ie INT test_files/test_5_grep.txt"
+    # "-echar test_files/test_1_grep.txt test_files/test_2_grep.txt"
+    # "-ne = -e out test_files/test_5_grep.txt"
+    # "-iv int test_files/test_5_grep.txt"
+    # "-in int test_files/test_5_grep.txt"
+    # "-c -l aboba test_files/test_1_grep.txt test_files/test_5_grep.txt"
+    # "-v test_files/test_1_grep.txt -e ank"
+    # "-noe "\)" test_files/test_5_grep.txt"
+    # "-l for test_files/test_1_grep.txt test_files/test_2_grep.txt"
+    # "-o -e int test_files/test_4_grep.txt"
+    # "-o -e int -e in -e i test_files/test_1_grep.txt"
+    # "-o -e in -e i -e int test_files/test_1_grep.txt"
+    # "-e = -e out test_files/test_5_grep.txt"
+    # "-noe ing -e as -e the -e not -e is test_files/test_6_grep.txt"
+    # "-e ing -e as -e the -e not -e is test_files/test_6_grep.txt"
+    # "-c -e . test_files/test_1_grep.txt -e '.'"
+    # "-l for no_file.txt test_files/test_2_grep.txt"
+    # "-e int -si no_file.txt grep.c no_file2.txt grep.h"
+    # "-si grep.c -f no_pattern.txt"
+    # "-f test_files/test_3_grep.txt test_files/test_5_grep.txt"
 )
 
 run_test() {
@@ -77,6 +77,8 @@ run_test() {
         # echo $"valgrind --leak-check=full --show-leak-kinds=all "${s21_command[@]}" $param 2>&1 >/dev/null | grep -ic -e \"LEAK SUMMARY:\" -e \"ERROR SUMMARY: [^0]\" >/dev/null"
         valgrind --leak-check=full --show-leak-kinds=all "${s21_command[@]}" $param 2>&1 >/dev/null | grep -ic -e "LEAK SUMMARY:" -e "ERROR SUMMARY: [^0]" >/dev/null
     else
+        "${s21_command[@]}" ${param} 2>/dev/null
+        "${sys_command[@]}" --color=never ${param} 2>/dev/null
         cmp -s <(
             "${s21_command[@]}" ${param} 2>/dev/null
             echo ${PIPESTATUS[0]}
@@ -109,38 +111,38 @@ for i in "${manual[@]}"; do
     run_test "$i"
 done
 
-printf "\n"
-echo "#######################"
-echo "AUTOTESTS"
-echo "#######################"
-printf "\n"
-echo "======================="
-echo "1 PARAMETER"
-echo "======================="
-printf "\n"
+# printf "\n"
+# echo "#######################"
+# echo "AUTOTESTS"
+# echo "#######################"
+# printf "\n"
+# echo "======================="
+# echo "1 PARAMETER"
+# echo "======================="
+# printf "\n"
 
-for var1 in "${flags[@]}"; do
-    for i in "${tests[@]}"; do
-        var="-$var1"
-        run_test "$i"
-    done
-done
-printf "\n"
-echo "======================="
-echo "2 PARAMETERS"
-echo "======================="
-printf "\n"
+# for var1 in "${flags[@]}"; do
+#     for i in "${tests[@]}"; do
+#         var="-$var1"
+#         run_test "$i"
+#     done
+# done
+# printf "\n"
+# echo "======================="
+# echo "2 PARAMETERS"
+# echo "======================="
+# printf "\n"
 
-for var1 in "${flags[@]}"; do
-    for var2 in "${flags[@]}"; do
-        if [ $var1 != $var2 ]; then
-            for i in "${tests[@]}"; do
-                var="-$var1 -$var2"
-                run_test "$i"
-            done
-        fi
-    done
-done
+# for var1 in "${flags[@]}"; do
+#     for var2 in "${flags[@]}"; do
+#         if [ $var1 != $var2 ]; then
+#             for i in "${tests[@]}"; do
+#                 var="-$var1 -$var2"
+#                 run_test "$i"
+#             done
+#         fi
+#     done
+# done
 
 printf "\n"
 echo "FAILED: $FAIL"

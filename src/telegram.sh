dd5fd06032d85c8051ee7c8c2cfe12963da4dedc
elidacon <elidacon@student.21-school.ru>
2023-11-24 06:43:52 +0000

    Add CI_JOB_NAME, remove test echos in telegram.sh

--- src/telegram.sh
+++ src/telegram.sh
@@ -1,9 +1,7 @@
 source ~/.bashrc
 TIME=10
-echo 'test'
-echo $TELEGRAM_USER_ID
 
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $CI_JOB_STATUS%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="$CI_JOB_NAME status: $CI_JOB_STATUS%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
